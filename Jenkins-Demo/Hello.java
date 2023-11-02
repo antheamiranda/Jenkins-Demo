aed8353c2cc4fb2f1b1ee7eec36d68feb40eded3
Anthea Miranda <91622200+antheamiranda@users.noreply.github.com>
2023-11-02 18:34:36 +0530

    Delete Jenkins-Demo/Hello.java

--- Jenkins-Demo/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello{
-    public static void main(String args[]){
-         System.out.println("Hello World");
-         System.out.println("I am testing my code using Jenkins");
-         System.out.println("I am testing my code using Jenkins again");
-         System.out.println("One more line of code");
-    }
-}
