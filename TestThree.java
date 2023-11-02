021c88ee17b88487498ea593318247267ed46d71
Anthea Miranda <91622200+antheamiranda@users.noreply.github.com>
2023-11-03 01:55:27 +0530

    Add files via upload

---
+++ TestThree.java
@@ -0,0 +1,17 @@
+package jenkins;
+
+public class TestThree {
+
+	public static void main(String[] args) {
+		int i=5;
+		if(i==5)
+		{
+			System.out.println("True, it is 5");
+		}
+		else
+		{
+			System.out.println("False, it is not 5");
+		}
+	}
+
+}
---
+++ TestTwo.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+public class TestTwo {
+
+	public static void main(String[] args) {
+		int i=0;
+		while(i<6)
+		{
+			System.out.println("1");
+			i=i+1;
+		}
+		System.out.println("Successful");
+	}
+}
